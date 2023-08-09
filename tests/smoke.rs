ee5bbce1a91e3b6a05d87b5b562c8a392dad0130
nooberfsh <nooberfsh@gmail.com>
2023-08-09 17:34:36 +0800

    add smoke test

---
+++ tests/smoke.rs
@@ -0,0 +1,20 @@
+use timely::WorkerConfig;
+
+#[tokio::test]
+async fn smoke() {
+    let config = ddquery::Config {
+        workers:4,
+        worker_config: WorkerConfig::default(),
+    };
+    let handle = ddquery::start(config).await.unwrap();
+
+    handle.create_input_and_trace("t1").await.unwrap();
+
+    handle.insert("t1", 1, 103).await.unwrap();
+    let d = handle.query_one("t1", 1).await.unwrap();
+    assert_eq!(d, Some(103));
+
+    handle.delete("t1", 1).await.unwrap();
+    let d = handle.query_one("t1", 1).await.unwrap();
+    assert_eq!(d, None);
+}
